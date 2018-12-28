20db41d7a7bc640ffe76c16d13c88d93f77bcc7d
ZhenlyChen <zhenlychen@foxmail.com>
2018-12-28 18:22:23 +0800

    home

---
+++ code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/home/HomeFragment.java
@@ -0,0 +1,28 @@
+package studio.xmatrix.coffee.ui.home;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import studio.xmatrix.coffee.R;
+import studio.xmatrix.coffee.databinding.HomeFragmentBinding;
+
+public class HomeFragment extends Fragment {
+    HomeFragmentBinding binding;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.inflate(LayoutInflater.from(getActivity()), R.layout.home_fragment, null, false);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return binding.getRoot();
+    }
+}
