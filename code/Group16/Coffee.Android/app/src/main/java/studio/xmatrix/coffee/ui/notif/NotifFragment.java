2e6712067d186a09075db8fce15395ad4bbedc1d
ZhenlyChen <zhenlychen@foxmail.com>
2018-12-29 13:43:14 +0800

    fix style

--- code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/notif/NotifFragment.java
+++ code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/notif/NotifFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import studio.xmatrix.coffee.R;
-import studio.xmatrix.coffee.databinding.HomeFragmentBinding;
 import studio.xmatrix.coffee.databinding.NotifFragmentBinding;
 
 public class NotifFragment extends Fragment {
--- code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/person/PersonActivity.java
+++ code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/person/PersonActivity.java
@@ -3,9 +3,7 @@ package studio.xmatrix.coffee.ui.person;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import studio.xmatrix.coffee.R;
 import studio.xmatrix.coffee.ui.BaseActionBarActivity;
 
