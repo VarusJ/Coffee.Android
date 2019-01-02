8187513d9ddb104d4ea6f6cd66a1862ca9fa25ec
CookiesChen <1109349604@qq.com>
2019-01-02 18:36:12 +0800

    valid

--- code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/admin/SignUpActivityHandler.java
+++ code/Group16/Coffee.Android/app/src/main/java/studio/xmatrix/coffee/ui/admin/SignUpActivityHandler.java
@@ -3,6 +3,7 @@ package studio.xmatrix.coffee.ui.admin;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.widget.Toast;
@@ -15,10 +16,10 @@ import studio.xmatrix.coffee.inject.Injectable;
 import javax.inject.Inject;
 import java.util.Objects;
 
-import static studio.xmatrix.coffee.data.service.resource.CommonResource.StatusError;
-import static studio.xmatrix.coffee.data.service.resource.CommonResource.StatusSuccess;
+import static studio.xmatrix.coffee.data.service.resource.CommonResource.*;
 
 public class SignUpActivityHandler implements Injectable {
+
     private SignUpActivity activity;
     private SignupActivityBinding binding;
     private boolean passwordVisibility;
@@ -30,7 +31,10 @@ public class SignUpActivityHandler implements Injectable {
         static String nullPassword = "请输入密码";
         static String errConfirm = "两次输入密码不一致";
         static String errNetwork = "请检查网络连接";
-
+        static String invalidEmail = "邮箱格式错误";
+        static String invalidUsername = "用户名格式错误";
+        static String existUsername = "用户名已存在";
+        static String existEmail = "邮箱已存在";
     }
 
     @Inject
@@ -50,6 +54,7 @@ public class SignUpActivityHandler implements Injectable {
     private void initView() {
         binding.signupButton.setOnClickListener(v -> signupEvent());
         binding.signupButton.getBackground().setAlpha(50);
+        binding.signupButtonCardView.getBackground().setAlpha(50);
 
         passwordVisibility = false;
         confirmVisibility = false;
@@ -64,14 +69,26 @@ public class SignUpActivityHandler implements Injectable {
         String username = binding.signupUsername.getText().toString();
         String password = binding.signupPassword.getText().toString();
         String confirm = binding.signupConfirm.getText().toString();
+        // 正则表达式
+//        String emailReg = "/^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$/";
+//        String usernameReg = "/^[a-zA-Z][a-zA-Z0-9_]{0,31}$/";
+
         if (email.length() == 0){
             Toast.makeText(activity, errMsg.nullEmail, Toast.LENGTH_SHORT).show();
             return;
         }
+//        else if(!email.matches(emailReg)){
+//            Toast.makeText(activity, errMsg.invalidEmail, Toast.LENGTH_SHORT).show();
+//            return;
+//        }
         if (username.length() == 0){
             Toast.makeText(activity, errMsg.nullUsername, Toast.LENGTH_SHORT).show();
             return;
         }
+//        else if (!username.matches(usernameReg)){
+//            Toast.makeText(activity, errMsg.invalidUsername, Toast.LENGTH_SHORT).show();
+//            return;
+//        }
         if (password.length() == 0){
             Toast.makeText(activity, errMsg.nullPassword, Toast.LENGTH_SHORT).show();
             return;
@@ -83,17 +100,31 @@ public class SignUpActivityHandler implements Injectable {
         viewModel.register(username, email, password).observe(activity, res -> {
             assert res != null;
             if (res.getStatus() == Status.SUCCESS){
-                if (Objects.requireNonNull(res.getData()).getState().equals(StatusSuccess)){
-                    activity.finish();
-                } else if (res.getData().getState().equals(StatusError)){
-                    Toast.makeText(activity, "", Toast.LENGTH_SHORT).show();
+                switch (Objects.requireNonNull(res.getData()).getState()){
+                    case StatusNotValidUsername:
+                    case StatusReservedUsername:
+                        Toast.makeText(activity, errMsg.invalidUsername, Toast.LENGTH_SHORT).show();
+                        break;
+                    case StatusNotValidEmail:
+                        Toast.makeText(activity, errMsg.invalidEmail, Toast.LENGTH_SHORT).show();
+                        break;
+                    case StatusExistUsername:
+                        Toast.makeText(activity, errMsg.existUsername, Toast.LENGTH_SHORT).show();
+                        break;
+                    case StatusExistEmail:
+                        Toast.makeText(activity, errMsg.existEmail, Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Intent intent = new Intent(activity.getBaseContext(), ValidActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("email", email);
+                        intent.putExtras(bundle);
+                        activity.startActivity(intent);
                 }
             } else if (res.getStatus() == Status.ERROR){
                 Toast.makeText(activity, errMsg.errNetwork, Toast.LENGTH_SHORT).show();
             }
         });
-        Intent intent = new Intent(activity.getBaseContext(), ValidActivity.class);
-        activity.startActivity(intent);
     }
 
     private void handlerPasswordVisibility() {
